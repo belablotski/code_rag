42dec86b1de3780511ceff5efbf9a6b057d1537a
Aliaksei Belablotski <beloblotskiy@gmail.com>
2025-05-05 19:58:20 -0700

    Chroma DB session class with HTTP client

--- rag_ui.py
+++ rag_ui.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 
 from functools import partial
 from turtle import width
@@ -9,9 +10,39 @@ from pywebio.output import *
 from pywebio.pin import *
 from pywebio.session import set_env
 
+import chromadb
+import openai
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.vectorstores import Chroma
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+class ChromaDBSession:
+    def __init__(self, collection_name: str, api_url: str = None):
+        logging.debug(f"Initializing ChromaDBSession with collection_name={collection_name}, api_url={api_url}")
+        self.api_url = api_url
+        self.collection_name = collection_name
+        self.client = None
+        self.collection = None
+
+    def __enter__(self):
+        logging.debug(f"Entering context with collection_name={self.collection_name}, api_url={self.api_url}")
+        self.client = chromadb.HttpClient(settings=chromadb.config.Settings(anonymized_telemetry=False))
+        self.collection = self.client.get_collection(self.collection_name)
+        logging.info(f"Connected to {self.collection_name} with {self.collection.count()} elements.")
+        return self.collection
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        logging.debug(f"Exiting context with collection_name={self.collection_name}, api_url={self.api_url}")
+        self.client = None
+        self.collection = None
+
 # Theme: dark, sketchy, minty, yeti
 @config(theme='dark', css_style='width: 100%')
-def chat(q: str = None, iter: int = 0, clear=True) -> None:
+def chat(collection, q: str = None, iter: int = 0, clear=True) -> None:
     set_env(output_max_width = '100%')
     with use_scope('chat', clear=clear):
         if q is None:
@@ -25,5 +56,8 @@ def chat(q: str = None, iter: int = 0, clear=True) -> None:
         ], size='90px')
 
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.DEBUG)
-    start_server(chat, port=8080, debug=True)
+    logging.getLogger().setLevel(logging.INFO)
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    with ChromaDBSession('code_index', 'localhost:8000') as collection:
+        chat_with_collection = partial(chat, collection=collection)
+        start_server(chat_with_collection, port=8080, debug=True)
